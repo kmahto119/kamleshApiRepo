97c968cb6153aa85dbf658546efd2a582057b0b1
kailash <kailash007ranchi@gmail.com>
2020-02-03 00:02:01 +0530

    change

---
+++ src/test/java/com/api/rootLib/ExentTestManager.java
@@ -0,0 +1,32 @@
+package com.api.rootLib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+
+public class ExentTestManager 
+{
+ static Map<Integer, ExtentTest> extentTestMap=new HashMap<Integer,ExtentTest>();
+ static ExtentReports extent= ExtentManager.getInstance(); 
+ public static synchronized ExtentTest getTest()
+ {
+	 return(ExtentTest)extentTestMap.get((int)(long)(Thread.currentThread().getId()));
+	 
+	 
+ }
+ 
+ 
+ public static synchronized ExtentTest startTest(String TestName)
+ {
+	 ExtentTest test=extent.createTest(TestName);
+	 extentTestMap.put((int)(long)(Thread.currentThread().getId()), test);
+	 return test;
+ }
+ public static synchronized void endTest()
+ {
+	 extent.flush();
+ }
+ 
+}
